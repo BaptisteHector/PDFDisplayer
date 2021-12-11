9d91ff53cca51369f47b693c9aba59bfefbbeb14
baptiste <baptiste.hector@epitech.eu>
2021-12-11 19:26:07 +0100

    Project initialisation

---
+++ PDFDisplayer/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PDFDisplayer.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index(string url)
+        {
+            ViewBag.url = url;
+            return View();
+        }
+
+    }
+}
